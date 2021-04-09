f80fa0f6a12d9bb83739c4828759e2c51d7856db
Романов Андрей Сергеевич <andrey_romanof@mail.ru>
2021-04-09 10:50:59 +0700

    Убрал разделитель в пост запросе

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Knetic/govaluate"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
@@ -25,7 +26,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/api/v1/health", returnStatus200).Methods(http.MethodHead)
-	myRouter.HandleFunc("/api/v1/arithmetic", countThings).Methods(http.MethodPost)
+	myRouter.HandleFunc("api/v1/arithmetic", countThings).Methods(http.MethodPost)
 	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
@@ -38,20 +39,19 @@ func countThings(w http.ResponseWriter, r *http.Request) {
 	log.Info("Request count things")
 	log.Info("Request = ", r.Body)
 	log.Info("Method = ", r.Method)
-	// expression, err := govaluate.NewEvaluableExpression("2 + 3 - 1")
-	// if err != nil {
-	// 	log.Info(err)
-	// }
-	// result, err := expression.Evaluate(nil)
-	// if err != nil {
-	// 	log.Info(err)
-	// }
-	// log.Info(result)
+	expression, err := govaluate.NewEvaluableExpression("2 + 3 - 1")
+	if err != nil {
+		log.Info(err)
+	}
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		log.Info(err)
+	}
+	log.Info(result)
 	// json.NewEncoder(w).Encode(expression)
 }
 
 func main() {
-	// log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 	log.Info("Rest API v2.0 - Mux Routers")
 
